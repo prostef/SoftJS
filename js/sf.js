80874eab2415a2de8a2e506f5f13fd0cf55824f2
Igor Bykov <con29rus@live.ru>
2015-08-08 01:20:30 +0300

    added alpha css setter and code formatting

--- js/sf.js
+++ js/sf.js
@@ -2,7 +2,7 @@ var sf = (function(){
 	
 	// Methods
 
-	var actions=function(nodeList){
+	var actions = function(nodeList){
 		this.test = function(){
 			sortOut(this,function(el){
 				console.log(el);
@@ -12,41 +12,49 @@ var sf = (function(){
 
 	// Setters and Getters
 
-	var mixins=function(obj){
+	var mixins = function(obj){
 
 		// inner
 
-		def.get(obj,'inner',function(){
-			return this.lentdh==1 ? this[0].innerHTML : (function(){
-				var inner=[];
-				sortOut(this,function(el){
+		def.get(obj, 'inner' ,function(){
+			return this.lentdh == 1 ? this[0].innerHTML : (function(){
+				var inner = [];
+				sortOut(this, function(el){
 					inner.push(el.innerHTML);
 				});
 				return inner.reverse();
 			})();
 		});
 
-		def.set(obj,'inner',function(str){
-			sortOut(this,function(el){
-				el.innerHTML=str;
+		def.set(obj, 'inner', function(str){
+			sortOut(this, function(el){
+				el.innerHTML = str;
 			});
 		});
 
 		// class
-		
-		def.get(obj,'class',function(){
-			return this.length==1 ? this[0].className : (function(){
-				var cls=[];
-				sortOut(this,function(el){
+
+		def.get(obj, 'class', function(){
+			return this.length == 1 ? this[0].className : (function(){
+				var cls = [];
+				sortOut(this, function(el){
 					cls.push(el.className);
 				});
 				return cls.reverse();
 			})();
 		});
 		
-		def.set(obj,'class',function(str){
-			sortOut(this,function(el){
+		def.set(obj, 'class', function(str){
+			sortOut(this, function(el){
 				el.setAttribute('class',str);
+			});
+		});
+
+		// css
+
+		def.set(obj, 'css', function(props){
+			sortOut(this, function(el){
+				el.style.cssText=props;
 			});
 		});
 
@@ -55,32 +63,30 @@ var sf = (function(){
 
 	// Private functions
 
-	var def={
-		get:function(obj,prop,func){
-			Object.defineProperty(obj,prop,{
+	var def = {
+		get:function(obj, prop, func){
+			Object.defineProperty(obj, prop, {
 				get:func,
-				configurable: true,
+				configurable:true,
 			});
 		},
-		set:function(obj,prop,func){
-			Object.defineProperty(obj,prop,{
+		set:function(obj, prop, func){
+			Object.defineProperty(obj, prop, {
 				set:func,
-				configurable: true,
+				configurable:true,
 			});
 		},
 	}
 
 	var sortOut = function (object, callback) {
-		tmp = [];
 		var i = object.length;
-		while (i--) tmp.push(callback(object[i]));
-		return tmp.reverse();
+		while(i--)callback(object[i]);
 	}
 
 	// General object
 
-	var sf=function(argument, callback){
-		var Objects=document.querySelectorAll(argument);
+	var sf = function(argument, callback){
+		var Objects = document.querySelectorAll(argument);
 		actions.prototype=Objects;
 		callback && sortOut(Objects, callback);
 		return mixins(new actions);
@@ -88,9 +94,9 @@ var sf = (function(){
 
 	// General methods
 
-	sf.test=function(){
+	sf.test = function(){
 		console.log(this);
 	}
 
 	return sf;
-})()
+})();
