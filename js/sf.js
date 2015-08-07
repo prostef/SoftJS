86b3d10092fb1e0e1e5b8d8719245f4bdd11a58d
Igor Bykov <con29rus@live.ru>
2015-08-07 12:58:54 +0300

    added setters and getters

--- js/sf.js
+++ js/sf.js
@@ -1,9 +1,51 @@
 var sf = (function(){
+	
+	// Methods
 
 	var actions=function(nodeList){
-		this.test = function(){}
+		this.test = function(){
+			sortOut(this,function(el){
+				console.log(el);
+			});
+		}
 	}
 
+	// Setters and Getters
+
+	var mixins=function(obj){
+		def.get(obj,'inner',function(){
+			var inner=[];
+			sortOut(this,function(el){
+				inner.push(el.innerHTML);
+			});
+			return inner.reverse().join(', ');
+		});
+
+		def.set(obj,'inner',function(str){
+			sortOut(this,function(el){
+				el.innerHTML=str;
+			});
+		});
+
+		return obj;
+	}
+
+	// Private functions
+
+	var def={
+		get:function(obj,prop,func){
+			Object.defineProperty(obj,prop,{
+				get:func,
+				configurable: true,
+			});
+		},
+		set:function(obj,prop,func){
+			Object.defineProperty(obj,prop,{
+				set:func,
+				configurable: true,
+			});
+		},
+	}
 
 	var sortOut = function (object, callback) {
 		tmp = [];
@@ -12,10 +54,15 @@ var sf = (function(){
 		return tmp.reverse();
 	}
 
+	// General object and methods
+
 	var sf=function(argument){
-		var v=document.querySelectorAll(argument);
-		actions.prototype=v;
-		return new actions;
+		var Objects=document.querySelectorAll(argument);
+		actions.prototype=Objects;
+		return mixins(new actions);
 	}
+
+
+
 	return sf;
 })()
