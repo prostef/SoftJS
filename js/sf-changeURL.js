a4a1a0ab4e2e0c404e26bde3e57b2be81ecc7b92
Igor Bykov <con29rus@icloud.com>
2016-01-14 17:04:01 +0300

    changet method changeUrl

--- js/sf-changeURL.js
+++ js/sf-changeURL.js
@@ -1,7 +1,4 @@
 sf.changeURL = function (url) {
-	var reg = /\/?\?(.*)$/;
-	(matches = url.match(reg)) && matches[1] ? (function(){
-		history.pushState(null, null, '?' + matches[1]);
-		history.replaceState(null, null, '?' + matches[1]);
-	})() : '';
+	history.pushState(null, null, url);
+	history.replaceState(null, null, url);
 }
