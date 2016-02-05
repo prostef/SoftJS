58ae2e417d22efb78147013dc071cdd71fe56298
Igor Bykov <con29rus@icloud.com>
2016-02-05 16:33:31 +0300

    fixed changeUrl method

--- js/sf-changeURL.js
+++ js/sf-changeURL.js
@@ -1,3 +1,11 @@
+sf.ready(function(){
+
+	sf(window).addEv('popstate', sf.popstate);
+
+});
+
+sf.popstate = function(){ window.location.reload(); }
+
 sf.changeURL = function (url) {
 	history.pushState(null, null, url);
 	history.replaceState(null, null, url);
